87dc4473a6ffc5350ba2ad74143db01cbf5910fc
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-11-22 11:29:12 -0500

    try this

--- extension.js
+++ extension.js
@@ -296,66 +296,16 @@ var _ConsoleWarn = console.warn;
 var _ConsoleError = console.error;
 var _ConsoleGroup = console.group;
 var _ConsoleDebug = console.debug;
-var fName = "";
 console.log = function () {
   args = [];
-  args.push("%c[" + fName + "] ");
-  args.push("color: #ae7337");
+  args.push("%c[INFO] ");
+  args.push(" ");
   // Note: arguments is part of the prototype
   for (var i = 0; i < arguments.length; i++) {
     args.push(arguments[i]);
   }
   _ConsoleLog.apply(console, args);
-};
-console.info = function () {
-  args = [];
-  args.push("%c[" + fName + "] ");
-  args.push("color: #ae7337");
-  // Note: arguments is part of the prototype
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-  _ConsoleInfo.apply(console, args);
-};
-console.warn = function () {
-  args = [];
-  args.push("%c[" + fName + "] ");
-  args.push("color: #ae7337");
-  // Note: arguments is part of the prototype
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-  _ConsoleWarn.apply(console, args);
-};
-console.error = function () {
-  args = [];
-  args.push("%c[" + fName + "] ");
-  args.push("color: #ae7337");
-  // Note: arguments is part of the prototype
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-  _ConsoleError.apply(console, args);
-};
-console.group = function () {
-  args = [];
-  args.push("%c[" + fName + "] ");
-  args.push("color: #ae7337");
-  // Note: arguments is part of the prototype
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-  _ConsoleGroup.apply(console, args);
-};
-console.debug = function () {
-  args = [];
-  args.push("%c[" + fName + "] ");
-  args.push("color: #ae7337");
-  // Note: arguments is part of the prototype
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-  _ConsoleDebug.apply(console, args);
+  o.appendLine(args.join(" "));
 };
 
 vscode.commands.registerCommand("AdamRaichu.devtools.evalInput", function () {
