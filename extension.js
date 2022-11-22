ced1aec1843722b12ab187e879a61226a122e0ac
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-11-22 08:38:52 -0500

    add functionality

--- extension.js
+++ extension.js
@@ -1,1 +1,15 @@
 const vscode = require("vscode");
+
+vscode.commands.registerCommand("AdamRaichu.devtools.eval", function () {
+  vscode.window
+    .showInputBox({
+      title: "Code to eval",
+      prompt: "USE WITH CAUTION",
+      placeHolder: 'console.log("Hello world")',
+    })
+    .then(function (code) {
+      console.log("Evaluating code...");
+      console.log(code);
+      eval(code);
+    });
+});
